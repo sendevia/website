d823026b48f953d7c6d1a4cf98df7963fa249f6f
sendevia <me@sendevia.top>
2024-07-12 22:27:58 +0800

    Initial

---
+++ .vitepress/theme/Layout.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { useData } from 'vitepress'
+
+// https://vitepress.dev/reference/runtime-api#usedata
+const { site, page, frontmatter } = useData()
+</script>
+
+<template>
+  <div v-if="frontmatter.home">
+    <h1>{{ site.title }}</h1>
+    <p>{{ site.description }}</p>
+    <ul>
+      <li><a href="/markdown-examples.html">Markdown Examples</a></li>
+      <li><a href="/api-examples.html">API Examples</a></li>
+    </ul>
+  </div>
+
+  <div v-if="page.isNotFound">
+    <a href="/">Home</a>
+    <p>404!</p>
+  </div>
+
+  <div v-else>
+    <a href="/">Home</a>
+    <Content />
+  </div>
+
+</template>
