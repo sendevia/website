94f253360e04796af18c879fade899c683a3139f
sendevia <me@sendevia.top>
2025-09-22 22:20:28 +0800

    初始化文章布局

---
+++ .vitepress/theme/layouts/Article.vue
@@ -0,0 +1,16 @@
+<template>
+  <article class="article-layout">
+    <header>
+      <h1>{{ frontmatter.title }}</h1>
+      <p v-if="frontmatter.date">发表于：{{ frontmatter.date }}</p>
+    </header>
+    <section>
+      <Content />
+    </section>
+  </article>
+</template>
+
+<script setup lang="ts">
+import { useGlobalData } from "../composables/useGlobalData";
+const { frontmatter } = useGlobalData();
+</script>
