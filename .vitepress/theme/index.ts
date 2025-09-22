517f38e3b20c8044f9f54cabb279447f9ed5d0a9
sendevia <me@sendevia.top>
2025-09-22 22:15:35 +0800

    优化默认布局并重命名

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,5 +1,4 @@
-// https://vitepress.dev/guide/custom-theme
-import Layout from "./Layout.vue";
+import Layout from "./layouts/Default.vue";
 import type { Theme } from "vitepress";
 import "./styles/main.scss";
 
---
+++ .vitepress/theme/layouts/Default.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { useGlobalData } from "../composables/useGlobalData";
+import AllPostsLayout from "./AllPosts.vue";
+import ArticleLayout from "./Article.vue";
+import Footer from "../components/Footer.vue";
+import NotFoundLayout from "./NotFound.vue";
+import SearchPostsLayout from "./SearchPosts.vue";
+import Sidebar from "../components/Sidebar.vue";
+const { site, page, frontmatter } = useGlobalData();
+</script>
+
+<template>
+  <div id="main-layout" spec="feed">
+    <Sidebar />
+    <div id="main-layout-content-flow">
+      <div id="main-layout-content-filler">
+        <div v-if="frontmatter.home">
+          <h1>{{ site.title }}</h1>
+          <p>{{ site.description }}</p>
+        </div>
+        <NotFoundLayout v-else-if="page.isNotFound" />
+        <AllPostsLayout v-else-if="frontmatter.layout === 'posts'" />
+        <SearchPostsLayout v-else-if="frontmatter.layout === 'search'" />
+        <ArticleLayout v-else />
+      </div>
+      <Footer />
+    </div>
+  </div>
+</template>
