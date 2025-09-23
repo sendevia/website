52ba69b0a42f707ae4164d29fa133d97427c009f
sendevia <me@sendevia.top>
2025-09-23 12:25:41 +0800

    增加404页面

---
+++ .vitepress/theme/layouts/NotFound.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="notfound">
+    <div id="notfound-information">
+      <h1>(⊙x⊙;)</h1>
+      <div>
+        <h3>你的网页遇到问题，不需要刷新。</h3>
+        <h3>我们不收集任何信息，你只需 <a href="/">返回本站首页</a>。</h3>
+      </div>
+      <h4>1000% 完成</h4>
+    </div>
+    <div id="notfound-qrcode">
+      <span></span>
+      <div>
+        <p>所访问页面不存在或已失效，请检查链接是否正确。</p>
+        <p>终止代码：404_NOT_FOUND</p>
+      </div>
+    </div>
+  </div>
+</template>
