3bce8e20ff47126e9a8260dcad61679505535ee8
sendevia <me@sendevia.top>
2025-11-09 15:59:55 +0800

    feat: add reusable Button component

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,10 +1,14 @@
-import Layout from "./layouts/Default.vue";
 import type { Theme } from "vitepress";
+
+import Layout from "./layouts/Default.vue";
+import Button from "./components/Button.vue";
+
 import "./styles/main.scss";
 
 export default {
   Layout,
   enhanceApp({ app }) {
-    app.component("MainLaylout", Layout);
+    app.component("MainLayout", Layout);
+    app.component("MaterialButton", Button);
   },
 } satisfies Theme;
