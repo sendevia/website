1e57cace8905a2def3f4b0ac72f2e9caf19dccf3
sendevia <me@sendevia.top>
2025-12-05 23:19:09 +0800

    feat(Footer): replace ID selectors with class selectors in Footer component

--- .vitepress/theme/components/Footer.vue
+++ .vitepress/theme/components/Footer.vue
@@ -6,17 +6,17 @@ const siteVersion = theme.value.siteVersion;
 </script>
 
 <template>
-  <footer class="main-layout-footer">
+  <footer class="Footer">
     <hr />
-    <div id="main-layout-footer-description">
-      <div id="main-layout-footer-description-topic">
+    <div class="description-area">
+      <div class="topic">
         <img src="/assets/images/avatar.webp" alt="logo" />
         <div>
           <h3>{{ page.title }}</h3>
           <p>来自 {{ site.title }}</p>
         </div>
       </div>
-      <div id="main-layout-footer-description-theme">
+      <div class="theme-info">
         <p>
           使用
           <a href="https://github.com/sendevia/vitepress-theme" target="_blank">sendevia's material theme</a>
@@ -24,14 +24,14 @@ const siteVersion = theme.value.siteVersion;
         </p>
         <a :href="'https://github.com/sendevia/website/tags/v' + siteVersion" target="_blank"> 版本：{{ siteVersion }}</a>
       </div>
-      <div id="main-layout-footer-description-beian">
-        <div id="main-layout-footer-beian-gongan">
+      <div class="beian-info">
+        <div class="beian-gongan">
           <img src="/assets/images/beian.png" loading="eager" />
           <a href="https://beian.mps.gov.cn/#/query/webSearch?code=23020002230215" target="_blank"
             >黑公网安备23020002230215</a
           >
         </div>
-        <div id="main-layout-footer-Registration-icp">
+        <div>
           <a href="https://beian.miit.gov.cn/" target="_blank">黑ICP备2024016516号-1</a>
         </div>
       </div>
