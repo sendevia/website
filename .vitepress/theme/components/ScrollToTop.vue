385eac5881a07b27dcf7c7e9146e58cfa8959d52
sendevia <me@sendevia.top>
2025-12-05 23:33:10 +0800

    feat(ScrollToTop): replace IDs with classes in ScrollToTop component

--- .vitepress/theme/components/ScrollToTop.vue
+++ .vitepress/theme/components/ScrollToTop.vue
@@ -4,14 +4,14 @@ import { useGlobalScroll } from "../composables/useGlobalScroll";
 const { isScrolled } = useGlobalScroll({ threshold: 500 });
 
 function scrollToTop() {
-  const container = document.getElementById("layout-content-flow");
+  const container = document.getElementById("contentFlow");
   (container as HTMLElement).scrollTo({ top: 0, behavior: "smooth" });
 }
 </script>
 
 <template>
-  <div id="layout-scrolltop" :class="{ visible: isScrolled }">
-    <span id="scrolltop-button" role="button" aria-label="Scroll to top" @click="scrollToTop"> arrow_upward </span>
+  <div class="ScrollToTop" :class="{ visible: isScrolled }">
+    <span class="button" role="button" aria-label="Scroll to top" @click="scrollToTop"> arrow_upward </span>
   </div>
 </template>
 
