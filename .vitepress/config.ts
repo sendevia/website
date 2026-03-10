2cbae870eb77db564568b7be15a966446a927ed8
sendevia <me@sendevia.top>
2026-03-10 23:02:52 +0800

    feat: replace Google Fonts Material Symbols with self-hosted font

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -76,13 +76,6 @@ export default defineConfig({
         rel: "stylesheet",
       },
     ],
-    [
-      "link",
-      {
-        href: "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200",
-        rel: "stylesheet",
-      },
-    ],
   ],
   metaChunk: true,
   lastUpdated: true,
