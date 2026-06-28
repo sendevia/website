d685142b77cc12c0e6d1ecca6c748f6f0a20a06b
sendevia <me@sendevia.top>
2026-06-28 04:32:15 +0000

    chore: 优化代码lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -24,6 +24,8 @@ export default tseslint.config(
       "vue/max-attributes-per-line": "off",
       "vue/singleline-html-element-content-newline": "off",
       "vue/html-self-closing": "off",
+      "vue/html-indent": "off",
+      "vue/multiline-html-element-content-newline": "off",
       "vue/attributes-order": "off",
       "vue/require-default-prop": "off",
       "vue/html-closing-bracket-newline": "off",
